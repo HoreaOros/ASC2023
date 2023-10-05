23d3d22f8e8a53a99f457a1167f322c57fb69635
Horea Oros <horea.oros@gmail.com>
2023-10-05 09:59:34 +0300

    refactor conversion

--- Conversii/Conversii4/Program.cs
+++ Conversii/Conversii4/Program.cs
@@ -12,9 +12,62 @@ namespace Conversii4
         static void Main(string[] args)
         {
             int n;
-            n = int.Parse(Console.ReadLine());
-            ConvertFromDecimalToBinary(n);
-            ConvertFromDecimalToHex(n);
+            try
+            {
+                n = int.Parse(Console.ReadLine());
+                //ConvertFromDecimalToBinary(n);
+                //ConvertFromDecimalToHex(n);
+                ConvertFromDecimal(n, 64);
+                
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ConvertFromDecimal(int n, int toBase)
+        {
+            if (toBase > 16)
+                throw new ArgumentException("toBase too large");
+
+            Stack<int> stack = new Stack<int>();
+            while (n > 0)
+            {
+                stack.Push(n % toBase);
+                n = n / toBase;
+            }
+            while (stack.Count > 0)
+            {
+                int d = stack.Pop();
+                if (d < 10)
+                    Console.Write(d);
+                else
+                {
+                    switch (d)
+                    {
+                        case 10:
+                            Console.Write("A");
+                            break;
+                        case 11:
+                            Console.Write("B");
+                            break;
+                        case 12:
+                            Console.Write("C");
+                            break;
+                        case 13:
+                            Console.Write("D");
+                            break;
+                        case 14:
+                            Console.Write("E");
+                            break;
+                        case 15:
+                            Console.Write("F");
+                            break;
+                    }
+                }
+            }
+            Console.WriteLine();
         }
 
         private static void ConvertFromDecimalToHex(int n)
